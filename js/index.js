f834b0b8f604d9008d65198e346ee78b4d42133b
Ye Shi <zoe.shiye@gmail.com>
2020-12-16 04:42:36 +1100

    add tasks

--- js/index.js
+++ js/index.js
@@ -1,17 +1,46 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-    })();
+// Initialize a new TaskManager with currentId set to 0
+const taskManager = new TaskManager(0);
+
+
+// Select the New Task Form
+const newTaskForm = document.querySelector('#newTaskForm');
+
+
+// Select the inputs
+const newTaskNameInput = document.querySelector('#newTaskNameInput');
+const newTaskDescription = document.querySelector('#newTaskDescription');
+const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+const newTaskDueDate = document.querySelector('#newTaskDueDate');
+
+
+// Form Validation 
+newTaskForm.addEventListener('submit', function(event) {
+
+
+
+  // Get the values of the inputs
+  const name = newTaskNameInput.value;
+  const description = newTaskDescription.value;
+  const assignedTo = newTaskAssignedTo.value;
+  const dueDate = newTaskDueDate.value;
+
+
+  // Add the task to the task manager
+  taskManager.addTask(name, description, assignedTo, dueDate);
+  console.log(taskManager.tasks);
+
+  if (newTaskForm.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  newTaskForm.classList.add('was-validated');
+  
+  
+
+
+})
+  // Clear the form
+  newTaskNameInput.value = '';
+  newTaskDescription.value = '';
+  newTaskAssignedTo.value = '';
+  newTaskDueDate.value = '';
