7e6d1ac88ec4e7135ed89cf1c3288176ffe2c3ca
Airone Dalisay <afddalisay@gmail.com>
2021-01-31 21:26:46 +1100

    added cards

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,47 +1,66 @@
 // Create the HTML for a task
+// const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => `
+//     <li class="project__item" data-task-id=${id}>
+//         <a href="#" class="project__link focus--box-shadow d-flex justify-content-around">
+//             <div class="project__wrapper d-flex align-items-center justify-content-between">
+//                 <div class="info-icon" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
+//                     <i class="fas fa-info-circle"></i>
+//                 </div>
+//                 <div class="project__element project__inform col-3" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescriptio">
+//                     <span class="project__inform-name">${name}</span>
+//                 </div>
+//                 <div class="project__element project__assignee col-2" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
+//                     <span class="project__assignee-name">${assignedTo}</span>
+//                 </div>
+//                 <div class="project__element project__date col-2 pl-3" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
+//                     <time class="date" datetime="2020-05-05T10:00:00">${dueDate}</time>
+//                 </div>
+//                 <div class="project__element project__status col-2 pl-5" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
+//                     <span class="status text-white font-weight-bold p-1 rounded ${status}">${status}</span>
+//                 </div>
+//                 <div class="project__element project__setting col-3 d-flex">
+//                     <button class="btn btn-sm btn-outline-success done-button p-1 d-flex justify-content-center align-self-center align-items-center ${status !== 'DONE' ? 'visible' : 'invisible'}">Mark As Done</button>
+//                     <button class="btn edit-btn focus--box-shadow d-flex align-items-center justify-content-center ml-3" type="button">
+//                         <img src="img/edit.png" alt="">
+//                     </button>
+//                     <button class="btn delete-btn focus--box-shadow-delete d-flex align-items-center justify-content-center ml-3 pr-2 mr-3" type="button">
+                        
+//                     </button>
+//                 </div>
+//             </div>
+//         </a>
+//         <div class="collapse" id="collapse${id}">
+//         <div class="card card-body discription">
+//           <p class="font-weight-bold lh-lg">Description</p>
+//           <p>${description}</p>
+//         </div>
+//        </div>
+//     </li>
+// `;
+
 const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => `
     <li class="project__item" data-task-id=${id}>
-        <a href="#" class="project__link focus--box-shadow d-flex justify-content-around">
-            <div class="project__wrapper d-flex align-items-center justify-content-between">
-                <div class="info-icon" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
-                    <i class="fas fa-info-circle"></i>
-                </div>
-                <div class="project__element project__inform col-3" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescriptio">
-                    <span class="project__inform-name">${name}</span>
-                </div>
-                <div class="project__element project__assignee col-2" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
-                    <span class="project__assignee-name">${assignedTo}</span>
-                </div>
-                <div class="project__element project__date col-2 pl-3" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
-                    <time class="date" datetime="2020-05-05T10:00:00">${dueDate}</time>
-                </div>
-                <div class="project__element project__status col-2 pl-5" data-toggle="collapse" href="#collapse${id}" role="button" aria-expanded="false" aria-controls="collapseTaskDescription">
-                    <span class="status text-white font-weight-bold p-1 rounded ${status}">${status}</span>
-                </div>
-                <div class="project__element project__setting col-3 d-flex">
-                    <button class="btn btn-sm btn-outline-success done-button p-1 d-flex justify-content-center align-self-center align-items-center ${status !== 'DONE' ? 'visible' : 'invisible'}">Mark As Done</button>
-                    <button class="btn edit-btn focus--box-shadow d-flex align-items-center justify-content-center ml-3" type="button">
-                        <img src="img/edit.png" alt="">
-                    </button>
-                    <button class="btn delete-btn focus--box-shadow-delete d-flex align-items-center justify-content-center ml-3 pr-2 mr-3" type="button">
-                        
-                    </button>
-                </div>
+        <a href="#" class="focus--box-shadow">
+        <div class="card bg-light mb-3" style="max-width: 18rem;">
+        <span class="border">
+            <div class="card-header">${name}</div>
+            <div class="card-body bg-white">
+                <p class="card-title">${assignedTo}</p>
+                <p class="card-text">${description}</p>
+                <p class="card-text">${status}</p>
+                <p class="card-text">${dueDate}</p>
             </div>
-        </a>
-        <div class="collapse" id="collapse${id}">
-        <div class="card card-body discription">
-          <p class="font-weight-bold lh-lg">Description</p>
-          <p>${description}</p>
+        </span>    
         </div>
-       </div>
+        </a>
     </li>
 `;
 
 
 
+
 // Create a TaskManager class
-export class TaskManager {
+class TaskManager {
     // Set up the tasks and currentId property in the contructor
     constructor(currentId = 0) {
         this.tasks = [];
@@ -161,7 +180,6 @@ export class TaskManager {
         // check if the currentId is saved in localStorage
         if (localStorage.getItem('currentId')) {
             // get the currentId string in localStorage
-            console.log('called load() check if any currentId are save in localStorage');
             const currentId = localStorage.getItem('currentId');
 
             // convert the currentId to a number and store it in our TaskManager
@@ -169,3 +187,4 @@ export class TaskManager {
         }
     } 
 }
+module.exports = TaskManager;
